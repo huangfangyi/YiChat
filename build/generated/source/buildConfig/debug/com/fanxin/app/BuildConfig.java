4654b520bcb6a6e64a49900e8a5d3d83cb160b28
huangfangyi <84543217@qq.com>
2016-06-26 00:40:14 +0800

    update packetbag name

---
+++ build/generated/source/buildConfig/debug/com/fanxin/app/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.fanxin.app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.fanxin.app";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 35;
+  public static final String VERSION_NAME = "3.0";
+}
