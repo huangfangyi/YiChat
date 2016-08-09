58f1a9dc2985be2bb0ef7736371444b4c7e476a7
huangfangyi <84543217@qq.com>
2016-08-09 17:53:58 +0800

    增加群成员资料获取并保存在缓存中

--- src/com/fanxin/app/main/service/GroupService.java
+++ src/com/fanxin/app/main/service/GroupService.java
@@ -2,13 +2,22 @@ package com.fanxin.app.main.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.support.annotation.Nullable;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.fanxin.app.main.FXConstant;
+import com.fanxin.app.main.db.ACache;
+import com.fanxin.app.main.utils.OkHttpManager;
+import com.fanxin.app.main.utils.Param;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMGroup;
 import com.hyphenate.exceptions.HyphenateException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,6 +25,20 @@ import java.util.List;
  * QQ:84543217
  */
 public class GroupService extends Service{
+    private Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if(msg.what==1000){
+                //JSONArray jsonArray= (JSONArray) msg.obj;
+                String groupId= (String) msg.obj;
+                getGroupMembersInServer(groupId);
+
+            }
+
+        }
+
+    };
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -39,12 +62,44 @@ public class GroupService extends Service{
                     List<EMGroup> grouplist = EMClient.getInstance().groupManager().getJoinedGroupsFromServer();//需异步处理
                     for(   EMGroup emGroup:   grouplist){
                         EMGroup group = EMClient.getInstance().groupManager().getGroupFromServer(emGroup.getGroupId());
+                        if(group!=null&&group.getGroupId()!=null){
+                            Message msg=handler.obtainMessage();
+                            msg.what=1000;
+                            msg.obj=group.getGroupId();
+                            msg.sendToTarget();
+                        }
                     }
                 } catch (HyphenateException e) {
                     e.printStackTrace();
                 }
             }
         }).start();
+
+    }
+
+
+    private  void   getGroupMembersInServer(final String groupId){
+
+        List<Param> params = new ArrayList<>();
+        params.add(new Param("groupId", groupId));
+        OkHttpManager.getInstance().post(params, FXConstant.URL_GROUP_MEMBERS, new OkHttpManager.HttpCallBack() {
+            @Override
+            public void onResponse(JSONObject jsonObject) {
+                int code = jsonObject.getIntValue("code");
+                if (code == 1000) {
+                    if (jsonObject.containsKey("data") && jsonObject.get("data") instanceof JSONArray) {
+                        JSONArray jsonArray = jsonObject.getJSONArray("data");
+                        ACache.get(getApplicationContext()).put(groupId,jsonArray);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(String errorMsg) {
+
+            }
+        });
+
 
     }
 
